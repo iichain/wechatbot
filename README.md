5c4ac76b4d1b66d6425ee2f28e5cc89836c6d250
just like before <agangdi@gmail.com>
2018-02-16 00:06:10 +0800

    git init

---
+++ README.md
@@ -0,0 +1,10 @@
+# wechatbot
+
+# dependence
+    python3
+    
+# install 
+    pip install -r requirements.txt
+    
+# run
+    python3 weixin.py
---
+++ config.py
@@ -0,0 +1,9 @@
+config = {
+    'mysql': {
+        'host': 'localhost',
+        'user': 'root',
+        'port': 3306,
+        'database': 'iccwx',
+        'password': ''
+    }
+}
